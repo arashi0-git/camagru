77665cbe338373a01ca6a523c62e1c9cd4b59149
arashi0-git <Y.arashi0408@icloud.com>
2026-06-06 20:37:45 +0900

    remove config file and add gitignore

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => '127.0.0.1',
-    'port' => '3307',
-    'db' => 'camagru',
-    'user' => 'camagru_user',
-    'pass' => 'camagru_pass',
-    'charset' => 'utf8mb4',
-];
